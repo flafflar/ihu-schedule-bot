84b03bee420b2b9926054157154ce3d6c7f9d970
flafflar <achmichail@gmail.com>
2021-11-24 14:22:09 +0200

    Created a simple word tokenizer

--- bot.py
+++ bot.py
@@ -2,3 +2,20 @@ knowledge = [
         ('ψηφιακή σχεδίαση', 'καραμπατζάκης', 1, 16, 20),
         ('θεωρίες μάθησης', 'τσινάκος', 2, 13, 15)
 ]
+
+def tokenizer(sentence):
+    tokens = []
+    token = ''
+    for c in sentence:
+        if c == ' ':
+            if token != '':
+                tokens.append(token)
+                token = ''
+        else:
+            token += c
+    if token != '': tokens.append(token)
+    return tokens
+
+while True:
+    sentence = input('> ')
+    print(tokenizer(sentence))
